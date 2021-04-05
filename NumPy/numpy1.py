0a64245f4a3b910f1695ba42648a72bf4d25b04f
단단한슬라임 <66393239+1409-pjs@users.noreply.github.com>
2021-04-05 20:42:51 +0900

    python

--- NumPy/numpy1.py
+++ NumPy/numpy1.py
@@ -24,3 +24,27 @@ print(arr.size)
 #reshape
 #말그대로 재구성 차원도 바꿔줄수잇음
 #size는 유지하되 구성을 바꿔줄수있다.
+arr = np.array([[1, 2, 3], [1, 2, 3]])
+arr = arr.reshape([6])
+print(arr.shape)
+
+#random array 생성
+arr = np.random.randn(8,8)  
+print(arr.shape)
+arr = arr.reshape([32, 2])
+print(arr.shape)
+
+#arr.ravel arr 의 차원을 1차원으로 바꿔줌
+#쫙 펼쳐서 1자로 나열해준다고 생각하면됨
+arr = arr.ravel()
+print(arr)
+
+#size 값을 모르고 안의 값을 유지하되 차원을 늘리고 싶을땐?
+#np.expand_dims(늘릴대상, 앞으로(0)or 뒤로(-1))
+arr = np.expand_dims(arr, -1)
+
+#np.zeros 0으로찬 배열 만들기
+#자매품 = ones
+#그럼 다른 수들은어케해?! 
+#ones * 5 하면 5로이루어진배열이 된다
+#np.arange(?) 0 부터 ?-1 만큼의 배열이 생성됨 (5, 9) 는 5부터 8까지
