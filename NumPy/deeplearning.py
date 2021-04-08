5ef55a87d730363c0e29bde2b3bcda48d510f05b
단단한슬라임 <66393239+1409-pjs@users.noreply.github.com>
2021-04-08 19:59:16 +0900

    deeplearning

---
+++ NumPy/deeplearning.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+EPOCHS = 5
+
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.flatten = tf.keras.layers.Flatten(input_shape=(28, 28))
+        self.d1 = tf.keras.layers.Dense(128, activation='sigmoid')
+        self.d2 = tf.keras.layers.Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
+    
+@tf.function
+def train_step(model, images, labels, loss_object, optimizer, train_loss, train_accuracy):
+    with tf.GradientTape() as tape:
+        predictions = model(images)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+@tf.function
+def test_step(model, images, labels, loss_object, test_loss, test_accuracy):
+    predictions = model(images)
+
+    t_loss = loss_object(labels, predictions)
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+
+#Import and organize dataset
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+#Create Model
+model = MyModel()
