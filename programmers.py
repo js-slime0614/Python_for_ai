6010a1b4802f1b0d3519bba97e3372b43adf1b82
단단한슬라임 <66393239+1409-pjs@users.noreply.github.com>
2021-04-15 21:15:45 +0900

    commit

--- programmers.py
+++ programmers.py
@@ -2,13 +2,16 @@ import numpy as np
 
 def solution(numbers):
     answer = []
-    list_set = list()
     for i in range(len(numbers)):
         for j in range(len(numbers)):
-            if i != j:
-                list_set.append(numbers[i] + numbers[j])
-    answer = set(list_set)
-    
+            if i != j :
+                answer.append(numbers[i] + numbers[j])
+    for i in range(len(answer)):
+        for j in range(len(answer)):
+            if answer[i] == answer[j] and i != j:
+                answer[j] = 0
+    answer.sort()
+    answer.remove(0)
     return answer
 numbers = [2,1,3,4,1]
 print(solution(numbers))
