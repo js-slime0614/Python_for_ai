e7c06e79fa4bc9cc052da6e4e242c007f8041c8b
단단한슬라임 <66393239+1409-pjs@users.noreply.github.com>
2021-05-17 16:02:10 +0900

    pandas

--- jkhj.py
+++ jkhj.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
+#make csv file in DeskTop
 data = [[1,2,3,4],[5,6,7,8]]
 df = pd.DataFrame(data)
 df.to_csv('C:\\Users\\user\\Desktop\\ddd.csv')
-#make csv file in DeskTop 
-for i in range(1, 6):
-    print("CSV")
-#"what"
