b5ff3216edac5f8884bfab2b063cbef5b64c3aac
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2021-10-14 16:58:37 +0900

    m

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-a = list()
-a.append(1)
-print(a)
-del a[0]
-a.append(1)
-print(a[0])
-a.append(2)
-print(a)
-a.remove(2)
-print(a)
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import json
+
+
+img_path = os.path.join(self.path, self.filenames[idx])
+        img = np.asarray_chkfinite(Image.open(img_path))
+        with open(os.path.join(self.json_path, self.filenames[idx]), 'r') as f:
+            json_data = json.load(f)
+        
+        for i in range(0,len(json_data['annotations'])):
+            point = np.array(json_data['annotations'][i]['points'])
+            label_image = cv2.polylines(img, [point], True)
+            label_image = cv2.fillPoly(label_image, [point], (200, 255, 200))
